d7197e973c8fd843c8c3b224fb4de40aa9b3d8ce
ppusda <ppusda1234@gmail.com>
2022-04-06 21:00:31 +0900

    Pr01_Pr04

---
+++ src/Pr01/Pr04/Pr01_my04.java
@@ -0,0 +1,45 @@
+package Pr01.Pr04;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Pr01_my04 {
+    private List solution(int inputInt, List inputTextList) {
+        String answer = "";
+        int cnt = 0;
+
+        List reverseTextList = new ArrayList();
+
+        // new StringBuilder().reverse()
+
+        while (cnt < inputInt){
+            String tmpText = "";
+            String reverseText = inputTextList.get(cnt).toString();
+            char[] reverseArray = reverseText.toCharArray();
+            for(int j = reverseArray.length-1; 0 <= j; j--){
+                tmpText += reverseArray[j];
+            }
+            reverseTextList.add(tmpText);
+            cnt++;
+        }
+
+        return reverseTextList;
+    }
+
+    public static void main(String[] args) {
+        Pr01_my04 pr = new Pr01_my04();
+        Scanner sc = new Scanner(System.in);
+
+        List inputTextList = new ArrayList();
+
+        int inputInt = sc.nextInt();
+        for(int i = 0; i < inputInt; i++){
+            inputTextList.add(sc.next());
+        }
+
+        for(Object str : pr.solution(inputInt, inputTextList)){
+            System.out.println(str);
+        }
+    }
+}
