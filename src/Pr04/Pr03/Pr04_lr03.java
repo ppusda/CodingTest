4c0fa7e9ef1f68c1b843e37e7eeb1c570a0eaf85
ppusda <ppusda1234@gmail.com>
2022-05-05 19:56:31 +0900

    Pr04_Pr03

---
+++ src/Pr04/Pr03/Pr04_lr03.java
@@ -0,0 +1,50 @@
+package Pr04.Pr03;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Pr04_lr03 {
+    private ArrayList<Integer> solution(Integer max, Integer cnt, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        int tail = 0;
+
+        Map<Integer, Integer> tmpMap = new HashMap();
+
+        for(int i = tail; i < cnt-1; i++){
+            tmpMap.put(arr[i], tmpMap.getOrDefault(arr[i], 0)+1);
+        }
+
+        for(int head = cnt-1; head < max; head++){
+            tmpMap.put(arr[head], tmpMap.getOrDefault(arr[head], 0)+1);
+            answer.add(tmpMap.size());
+
+            tmpMap.put(arr[tail], tmpMap.get(arr[tail])-1);
+            if(tmpMap.get(arr[tail]) == 0){
+                tmpMap.remove(arr[tail]);
+            }
+            tail++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Pr04_lr03 pr = new Pr04_lr03();
+        Scanner sc = new Scanner(System.in);
+
+        int max = sc.nextInt();
+        int cnt = sc.nextInt();
+        int[] arr = new int[max];
+        for(int i = 0; i < max; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for (int num : pr.solution(max, cnt, arr)) {
+            System.out.print(num + " ");
+        }
+    }
+
+}
---
+++ src/Pr04/Pr03/Pr04_my03.java
@@ -0,0 +1,52 @@
+package Pr04.Pr03;
+
+import java.util.*;
+
+public class Pr04_my03 {
+    private ArrayList<Integer> solution(Integer max, Integer cnt, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        int head = cnt-1, tail = 0;
+
+        Map<Integer, Integer> tmpMap = new HashMap();
+
+        for(int i = tail; i <= head; i++){
+            tmpMap.put(arr[i], tmpMap.getOrDefault(arr[i], 0)+1);
+        }
+
+        while(head < max){
+            answer.add(tmpMap.size());
+
+            head++;
+            if(head == max){
+                break;
+            }
+            tmpMap.put(arr[head], tmpMap.getOrDefault(arr[head], 0)+1);
+            if(tmpMap.get(arr[tail]) == 1){
+                tmpMap.remove(arr[tail]);
+            }else{
+                tmpMap.put(arr[tail], tmpMap.get(arr[tail])-1);
+            }
+            tail++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Pr04_my03 pr = new Pr04_my03();
+        Scanner sc = new Scanner(System.in);
+
+        int max = sc.nextInt();
+        int cnt = sc.nextInt();
+        int[] arr = new int[max];
+        for(int i = 0; i < max; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for (int num : pr.solution(max, cnt, arr)) {
+            System.out.print(num + " ");
+        }
+    }
+
+}
