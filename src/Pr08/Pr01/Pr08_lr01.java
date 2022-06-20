7259acb32eae8e098be7f0550a93d309470a42bd
ppusda <ppusda1234@gmail.com>
2022-06-20 22:28:11 +0900

    DFS - Pr08_Pr01 합이 같은 부분집합

---
+++ src/Pr08/Pr01/Pr08_lr01.java
@@ -0,0 +1,39 @@
+package Pr08.Pr01;
+
+import java.util.Scanner;
+
+public class Pr08_lr01 {
+    static String answer = "NO";
+    static int n, total = 0;
+    boolean flag = false;
+    public void DFS(int L, int sum, int[] arr){
+        if(flag){
+            return;
+        }
+        if(L == n){
+            if((total - sum) == sum){
+                answer = "YES";
+                flag = true;
+            }
+        }else{
+            DFS(L+1, sum + arr[L], arr);
+            DFS(L+1, sum, arr);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Pr08_lr01 pr = new Pr08_lr01();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+            total += arr[i];
+        }
+
+        pr.DFS(0, 0, arr);
+        System.out.println(answer);
+    }
+}
---
+++ src/Pr08/Pr01/Pr08_my01.java
@@ -0,0 +1,39 @@
+package Pr08.Pr01;
+
+import java.util.Scanner;
+
+public class Pr08_my01 {
+    static String answer = "NO";
+    static int n, total;
+    static int[] ch, nist;
+    public void DFS(int index, int sum){
+        if(index >= n){
+            return;
+        }
+        if(total - sum == sum){
+            answer = "YES";
+        }else{
+            ch[index] = 1;
+            DFS(index+1, sum + nist[index]);
+
+            ch[index] = 0;
+            DFS(index+1, sum);
+        }
+    }
+
+    public static void main(String[] args) {
+        Pr08_my01 pr = new Pr08_my01();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        ch = new int[n];
+        nist = new int[n];
+        for(int i = 0; i < n; i++){
+            int tmp = sc.nextInt();
+            nist[i] = tmp;
+            total += tmp;
+        }
+
+        pr.DFS(0, 0);
+        System.out.println(answer);
+    }
+}
