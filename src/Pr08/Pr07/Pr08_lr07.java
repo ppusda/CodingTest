2575e4a4d474f3bac06e9a97235443bc63d1535a
ppusda <ppusda1234@gmail.com>
2022-06-27 23:12:39 +0900

    DFS - Pr08_Pr07 조합수 (메모지네이션)

---
+++ src/Pr08/Pr07/Pr08_lr07.java
@@ -0,0 +1,30 @@
+package Pr08.Pr07;
+
+import java.util.*;
+
+public class Pr08_lr07 {
+    private static int answer = 0;
+    private static int[][] chk = new int[35][35]; // 최대 값 설정
+
+    private int DFS(int n, int r) {
+        if (chk[n][r] > 0){
+            return chk[n][r];
+        }
+        if(n == r || r == 0){
+            return 1; // 3C3 혹은 3C0 같은 경우는 1이기에
+        }else{
+            return chk[n][r] = DFS(n-1, r-1) + DFS(n-1, r); // 조합 수 계산 결과 return
+        }
+    }
+
+    public static void main(String[] args) {
+        int n, r;
+        Pr08_lr07 pr = new Pr08_lr07();
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        r = sc.nextInt();
+
+        System.out.println(pr.DFS(n, r));
+    }
+}
---
+++ src/Pr08/Pr07/Pr08_my07.java
@@ -0,0 +1,45 @@
+package Pr08.Pr07;
+
+import java.util.Scanner;
+
+public class Pr08_my07 {
+    private static int answer = 0;
+    private static int[][] chk;
+
+    private void DFS(int n, int r) {
+        if(n <= 0 || r < 0){
+            return;
+        }
+        if(chk[n][r] == 1){
+            return;
+        }else{
+            chk[n][r] = 1;
+            answer ++;
+            DFS(n-1, r-1);
+            DFS(n-1, r);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int n, r;
+        Pr08_my07 pr = new Pr08_my07();
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        r = sc.nextInt();
+
+        chk = new int[n+1][r+1];
+
+        pr.DFS(n, r);
+        System.out.println(answer);
+    }
+}
+
+/**
+ * 문제를 잘못 이해하였다.
+ * 3C5 하위의 수열의 개수를 구하는 것인줄 알았는데,
+ * 조합식의 계산 결과를 재귀를 이용하여 푸는 것이었다.
+ *
+ * lr에서 살펴보자.
+ */
