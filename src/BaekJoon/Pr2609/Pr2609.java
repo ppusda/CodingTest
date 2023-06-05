2f19b8cb40126b4d1774f5cf5b30a5c630dd4e83
ppusda <ppusda1234@gmail.com>
2023-06-06 02:51:01 +0900

    BaekJoon: Pr2609

---
+++ src/BaekJoon/Pr2609/Pr2609.java
@@ -0,0 +1,34 @@
+package BaekJoon.Pr2609;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Pr2609 {
+    private static int GCD(int n, int k) {
+        if(k == 0) {
+            return n;
+        }else{
+            return GCD(k, n % k);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()); // 공백으로 들어올 시 처리방법
+
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+
+        // GCD 최대공약수 (유클리드 호제법)
+        int gcd = GCD(num1, num2);
+
+        // LCM 최소공배수
+        int lcm = (num1 * num2) / gcd;
+
+        // Result
+        System.out.println(gcd);
+        System.out.println(lcm);
+    }
+}
