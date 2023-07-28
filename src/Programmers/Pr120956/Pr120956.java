2b5a722d602dd2067178b85bd89ee59435e17cb5
ppusda <ppusda1234@gmail.com>
2023-07-28 20:12:10 +0900

    Programmers: 120956_옹알이

---
+++ src/Programmers/Pr120956/Pr120956.java
@@ -0,0 +1,30 @@
+package Programmers.Pr120956;
+
+public class Pr120956 {
+    public static String[] strArr = new String[]{"aya", "ye", "woo", "ma"};
+
+    public static int solution(String[] babbling) {
+        int answer = 0;
+
+        for(String str: babbling){
+            String tmp = str;
+            int cnt = 0;
+            while(cnt < strArr.length) {
+                if(tmp.contains(strArr[cnt])) {
+                    tmp = tmp.replace(strArr[cnt], "1");
+                }
+                cnt++;
+                if(tmp.matches("[0-9]+")) {
+                    answer ++;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new String[]{"ayaye", "uuuma", "ye", "yemawoo", "ayaa"}));
+    }
+}
---
+++ src/Programmers/Pr120956/Pr120956_Sol.java
@@ -0,0 +1,17 @@
+package Programmers.Pr120956;
+
+public class Pr120956_Sol {
+    public static int solution(String[] babbling) {
+        int answer = 0;
+        for(int i=0; i<babbling.length; i++){
+            if(babbling[i].matches("^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$")){
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new String[]{"ayaye", "uuuma", "ye", "yemawoo", "ayaa"}));
+    }
+}
