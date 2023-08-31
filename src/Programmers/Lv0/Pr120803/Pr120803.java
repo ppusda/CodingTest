05a16d17c597cdfff61629d61a8fefede162eeaa
ppusda <ppusda1234@gmail.com>
2023-08-31 19:51:01 +0900

    Programmers: 120803_두 수의 차

---
+++ src/Programmers/Lv0/Pr120803/Pr120803.java
@@ -0,0 +1,7 @@
+package Programmers.Lv0.Pr120803;
+
+public class Pr120803 {
+    public int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+}
---
+++ src/Programmers/Lv0/Pr120804/Pr120804.java
@@ -0,0 +1,7 @@
+package Programmers.Lv0.Pr120804;
+
+public class Pr120804 {
+    public int solution(int num1, int num2) {
+        return num1 * num2;
+    }
+}
