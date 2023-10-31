fb357755b67c9fc8af2fdd3d5d03200b6242a2ba
ppusda <ppusda1234@gmail.com>
2023-10-31 10:31:13 +0900

    Programmers: 120810_나머지 구하기

--- src/Programmers/Lv0/Pr120810/Pr120810.java
+++ src/Programmers/Lv0/Pr120810/Pr120810.java
@@ -1,2 +1,7 @@
-package Programmers.Lv0.Pr120810;public class Pr120810 {
+package Programmers.Lv0.Pr120810;
+
+public class Pr120810 {
+    public int solution(int num1, int num2) {
+        return num1 % num2;
+    }
 }
