7aa64570cb096aec0edb19e6e0c47c4778e63c61
ppusda <ppusda1234@gmail.com>
2024-01-19 15:23:34 +0900

    Programmers: 120806_두 수의 나눗셈

---
+++ src/Programmers/Lv0/Pr120806/Pr120806.java
@@ -0,0 +1,7 @@
+package Programmers.Lv0.Pr120806;
+
+public class Pr120806 {
+    public int solution(int num1, int num2) {
+        return (int)(((float)num1/num2)*1000); // (num1 * 1000) / num2
+    }
+}
