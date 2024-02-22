c8ea98c5b73b575e4acc81ea64e5a5792da43107
ppusda <ppusda1234@gmail.com>
2024-02-22 11:30:51 +0900

    Programmers: 120809_배열 두 배 만들기

---
+++ src/Programmers/Lv0/Pr120809/Pr120809.java
@@ -0,0 +1,13 @@
+package Programmers.Lv0.Pr120809;
+
+import java.util.Arrays;
+
+public class Pr120809 {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            answer[i] = numbers[i] * 2;
+        }
+        return answer;
+    } // return Arrays.stream(numbers).map(i -> i * 2).toArray();
+}
