3a0d77214c4867be3c5b73ffb6238764ffde19b5
ppusda <ppusda1234@gmail.com>
2024-02-24 20:35:50 +0900

    Programmers: 120826_특정 문자 제거하기

---
+++ src/Programmers/Lv0/Pr120826/Pr120826.java
@@ -0,0 +1,7 @@
+package Programmers.Lv0.Pr120826;
+
+public class Pr120826 {
+    public String solution(String my_string, String letter) {
+        return my_string.replaceAll(letter, "");
+    }
+}
