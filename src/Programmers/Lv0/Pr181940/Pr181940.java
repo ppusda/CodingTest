75b1afd8378893f0360e8cb8c3fb1ede1b056ded
ppusda <ppusda1234@gmail.com>
2024-03-23 22:50:56 +0900

    Programmers: 181940_문자열 곱하기

---
+++ src/Programmers/Lv0/Pr181940/Pr181940.java
@@ -0,0 +1,7 @@
+package Programmers.Lv0.Pr181940;
+
+public class Pr181940 {
+    public String solution(String my_string, int k) {
+        return my_string.repeat(k);
+    }
+} // StringBuilder 로 풀다가 String에 repeat 있는 것을 발견
