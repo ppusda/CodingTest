14b73b2f4fc6ae76819a427b420856c6f6d695e7
ppusda <ppusda1234@gmail.com>
2024-03-31 00:14:23 +0900

    Programmers: 181929_원소들의 곱과 합

--- src/Programmers/Lv0/Pr181929/Pr181929.java
+++ src/Programmers/Lv0/Pr181929/Pr181929.java
@@ -1,2 +1,16 @@
-package Programmers.Lv0.Pr181929;public class Pr181929 {
+// Programmers: 181929_원소들의 곱과 합
+package Programmers.Lv0.Pr181929;
+
+public class Pr181929 {
+    public int solution(int[] num_list) {
+        int sum = 0;
+        int mul = 1;
+
+        for (int n : num_list) {
+            mul *= n;
+            sum += n;
+        }
+
+        return mul < Math.pow(sum, 2) ? 1 : 0;
+    }
 }
