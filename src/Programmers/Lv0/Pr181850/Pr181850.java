7e6ee2ed26aa55f562250b70da1044f1cf1ff97d
ppusda <ppusda1234@gmail.com>
2024-03-31 00:03:50 +0900

    Programmers: 181850_정수 부분

---
+++ src/Programmers/Lv0/Pr181850/Pr181850.java
@@ -0,0 +1,7 @@
+package Programmers.Lv0.Pr181850;
+
+public class Pr181850 {
+    public int solution(double flo) {
+        return (int) flo;
+    }
+}
---
+++ src/Programmers/Lv0/Pr181929/Pr181929.java
@@ -0,0 +1,2 @@
+package Programmers.Lv0.Pr181929;public class Pr181929 {
+}
