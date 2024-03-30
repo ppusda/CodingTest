ddc3bf94870ad6686e64b13b1497869920838303
ppusda <ppusda1234@gmail.com>
2024-03-30 21:43:23 +0900

    Programmers: 181946_문자열 붙여서 출력하기

---
+++ src/Programmers/Lv0/Pr181946/Pr181946.java
@@ -0,0 +1,12 @@
+package Programmers.Lv0.Pr181946;
+
+import java.util.Scanner;
+
+public class Pr181946 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a+b);
+    }
+}
