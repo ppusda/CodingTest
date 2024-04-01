2f2bd7796f75686a0eaebff9236a1f350e9fa713
ppusda <ppusda1234@gmail.com>
2024-04-02 03:36:22 +0900

    Programmers: 181849_문자열 정수의 합

--- src/Programmers/Lv0/Pr181849/Pr181849.java
+++ src/Programmers/Lv0/Pr181849/Pr181849.java
@@ -1,2 +1,13 @@
-package Programmers.Lv0.Pr181849;public class Pr181849 {
+// Programmers: 181849_문자열 정수의 합
+package Programmers.Lv0.Pr181849;
+
+public class Pr181849 {
+    public int solution(String num_str) {
+        int answer = 0;
+        for (String num: num_str.split("")) {
+            answer += Integer.parseInt(num);
+        }
+
+        return answer;
+    }
 }
