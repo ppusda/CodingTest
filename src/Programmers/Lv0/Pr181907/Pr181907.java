61a4c68c66a362311b5caee419315626ac44e667
ppusda <ppusda1234@gmail.com>
2024-04-03 13:47:50 +0900

    Programmers: 181907_문자열의 앞의 n글자

--- src/Programmers/Lv0/Pr181907/Pr181907.java
+++ src/Programmers/Lv0/Pr181907/Pr181907.java
@@ -1,2 +1,8 @@
-package Programmers.Lv0.Pr181907;public class Pr181907 {
+// Programmers: 181907_문자열의 앞의 n글자
+package Programmers.Lv0.Pr181907;
+
+public class Pr181907 {
+    public String solution(String my_string, int n) {
+        return my_string.substring(0, n);
+    }
 }
