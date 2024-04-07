50cb2b54567047938cf415ec23a9baa5b4c753ef
ppusda <ppusda1234@gmail.com>
2024-04-08 01:47:02 +0900

    Programmers: 181840_정수 찾기

--- src/Programmers/Lv0/Pr181840/Pr181840.java
+++ src/Programmers/Lv0/Pr181840/Pr181840.java
@@ -1,2 +1,12 @@
-package Programmers.Lv0.Pr181840;public class Pr181840 {
+// Programmers: 181840_정수 찾기
+package Programmers.Lv0.Pr181840;
+
+import java.util.Arrays;
+
+public class Pr181840 {
+    class Solution {
+        public int solution(int[] num_list, int n) {
+            return Arrays.stream(num_list).anyMatch(i -> i == n) ? 1 : 0;
+        }
+    }
 }
