99cf4d4a93418557a9e76bbcf15301dc6185b846
ppusda <ppusda1234@gmail.com>
2024-04-09 00:54:02 +0900

    Programmers: 181876_소문자로 바꾸기

--- src/Programmers/Lv0/Pr181876/Pr181876.java
+++ src/Programmers/Lv0/Pr181876/Pr181876.java
@@ -1,2 +1,8 @@
-package Programmers.Lv0.Pr181876;public class Pr181876 {
+// Programmers: 181876_소문자로 바꾸기
+package Programmers.Lv0.Pr181876;
+
+public class Pr181876 {
+    public String solution(String myString) {
+        return myString.toLowerCase();
+    }
 }
