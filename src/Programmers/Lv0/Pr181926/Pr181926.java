255df67bd5dd7c79d2a49035e2295a2a96282fac
ppusda <ppusda1234@gmail.com>
2024-04-10 19:24:46 +0900

    Programmers: 181926_수 조작하기 1

--- src/Programmers/Lv0/Pr181926/Pr181926.java
+++ src/Programmers/Lv0/Pr181926/Pr181926.java
@@ -1,2 +1,17 @@
-package Programmers.Lv0.Pr181926;public class Pr181926 {
+// Programmers: 181926_수 조작하기 1
+package Programmers.Lv0.Pr181926;
+
+public class Pr181926 {
+    public int solution(int n, String control) {
+        for (String str: control.split("")) {
+            switch (str) {
+                case "w" -> n++;
+                case "s" -> n--;
+                case "d" -> n += 10;
+                case "a" -> n -= 10;
+            }
+        }
+
+        return n;
+    }
 }
