1dc695ee3de0c5affef38b0d8fc896757c407897
ppusda <ppusda1234@gmail.com>
2024-04-15 17:16:43 +0900

    Programmers: 181843_부분 문자열인지 확인하기

---
+++ src/Programmers/Lv0/Pr181843/Pr181843.java
@@ -0,0 +1,8 @@
+// Programmers: 181843_부분 문자열인지 확인하기
+package Programmers.Lv0.Pr181843;
+
+public class Pr181843 {
+    public int solution(String my_string, String target) {
+        return my_string.contains(target) ? 1 : 0;
+    }
+}
