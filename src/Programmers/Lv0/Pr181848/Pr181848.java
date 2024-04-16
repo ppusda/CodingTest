2a3b056ed1ce088fb9212d70ed0cf2433384d597
ppusda <ppusda1234@gmail.com>
2024-04-16 23:06:52 +0900

    Programmers: 181848_문자열을 정수로 변환하기

---
+++ src/Programmers/Lv0/Pr181848/Pr181848.java
@@ -0,0 +1,8 @@
+// Programmers: 181848_문자열을 정수로 변환하기
+package Programmers.Lv0.Pr181848;
+
+public class Pr181848 {
+    public int solution(String n_str) {
+        return Integer.parseInt(n_str);
+    }
+}
