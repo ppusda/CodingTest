642456cd5374ac0af336ab84bc2411b448e5e7d1
ppusda <ppusda1234@gmail.com>
2024-05-26 12:55:48 +0900

    LeetCode: 2336_Smallest-number-in-infinite-set

---
+++ src/LeetCode/Pr2336/Pr2336.java
@@ -0,0 +1,28 @@
+package LeetCode.Pr2336;
+
+import java.util.PriorityQueue;
+
+class SmallestInfiniteSet {
+    private final PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+    public SmallestInfiniteSet() {
+        for (int i = 1; i <= 1000; i++) {
+            this.priorityQueue.add(i);
+        }
+    }
+
+    public int popSmallest() {
+        if (this.priorityQueue.isEmpty()) {
+            throw new RuntimeException();
+        }
+
+        return this.priorityQueue.poll();
+    }
+
+    public void addBack(int num) {
+        if (!priorityQueue.contains(num)) {
+            priorityQueue.add(num);
+        }
+    }
+
+}
