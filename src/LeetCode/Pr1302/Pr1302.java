b19c83713b5d9bc8c0a81940644041c09ddbe4bd
ppusda <ppusda1234@gmail.com>
2024-06-01 16:53:39 +0900

    LeetCode: 1302_Deepest-leaves-sum

---
+++ src/LeetCode/Pr1302/Pr1302.java
@@ -0,0 +1,30 @@
+// LeetCode: 1302_Deepest-leaves-sum
+package LeetCode.Pr1302;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Pr1302 {
+    public int deepestLeavesSum(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int answer = 0;
+        while (!queue.isEmpty()) {
+            int levelSum = 0;
+            for (int i = 0; i < queue.size(); i++) {
+                TreeNode treeNode = queue.poll();
+                levelSum += treeNode.val;
+                if (treeNode.left != null) {
+                    queue.offer(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    queue.offer(treeNode.right);
+                }
+            }
+            answer = levelSum;
+        }
+
+        return answer;
+    }
+
+}
---
+++ src/LeetCode/Pr1302/TreeNode.java
@@ -0,0 +1,20 @@
+package LeetCode.Pr1302;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
