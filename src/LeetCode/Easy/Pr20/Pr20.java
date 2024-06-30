db8fbf9c7439de35e415488ab9eafe90c6395b9d
ppusda <ppusda1234@gmail.com>
2024-06-30 15:17:04 +0900

    LeetCode: 20_valid-parentheses

---
+++ src/LeetCode/Easy/Pr20/Pr20.java
@@ -0,0 +1,40 @@
+package LeetCode.Easy.Pr20;
+
+import java.util.Stack;
+
+public class Pr20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+            switch (ch) {
+                case ')':
+                    if (stack.isEmpty() || stack.peek() != '(') {
+                        return false;
+                    }
+                    stack.pop();
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.peek() != '[') {
+                        return false;
+                    }
+                    stack.pop();
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.peek() != '{') {
+                        return false;
+                    }
+                    stack.pop();
+                    break;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Pr20().isValid("]"));
+    }
+}
---
+++ src/LeetCode/Easy/Pr20/Pr20_Sol.java
@@ -0,0 +1,27 @@
+package LeetCode.Easy.Pr20;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Pr20_Sol {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> mapping = new HashMap<>();
+        mapping.put(')', '(');
+        mapping.put('}', '{');
+        mapping.put(']', '[');
+
+        for (char c : s.toCharArray()) {
+            if (mapping.containsValue(c)) {
+                stack.push(c);
+            } else if (mapping.containsKey(c)) {
+                if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
