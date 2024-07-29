30a64b6c9b930c6ecc2cf78813b13567d56f8c14
ppusda <ppusda1234@gmail.com>
2024-07-29 18:21:33 +0900

    LeetCode: 1475_final-prices-with-a-special-discount-in-a-shop

---
+++ src/LeetCode/Easy/Pr1475/Pr1475.java
@@ -0,0 +1,24 @@
+package LeetCode.Easy.Pr1475;
+
+public class Pr1475 {
+    public int[] finalPrices(int[] prices) {
+        int size = prices.length;
+        int[] answer = new int[size];
+        int index = 0;
+
+        for (int i = 0; i < size-1; i++) {
+            int price = prices[i];
+            for (int j = i+1; j < size; j++) {
+                if (price >= prices[j]) {
+                    price -= prices[j];
+                    break;
+                }
+            }
+            answer[index++] = price;
+        }
+
+        answer[index] = prices[size-1];
+        return answer;
+    }
+}
+}
