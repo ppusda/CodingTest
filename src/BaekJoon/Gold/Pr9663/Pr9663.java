8204b9a93645f39976e2b2d337d94417c6eb8f97
ppusda <ppusda1234@gmail.com>
2024-08-25 13:52:45 +0900

    Baekjoon: 9663_N-Queen - 설명 추가

--- src/BaekJoon/Gold/Pr9663/Pr9663.java
+++ src/BaekJoon/Gold/Pr9663/Pr9663.java
@@ -19,13 +19,13 @@ public class Pr9663 {
     }
 
 
-    private static void backtracking(int depth, int n) {
-        if (depth > n) {
+    private static void backtracking(int depth, int n) { // depth 는 세로 위치
+        if (depth > n) { // depth가 더 커지면 종료
             answer++;
             return;
         }
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) { // 가로 어디에 놓을 건지 확인
             col[depth] = i;  // 퀸을 놓음
             if (promising(depth)) {
                 backtracking(depth + 1, n);
@@ -35,7 +35,7 @@ public class Pr9663 {
 
     private static boolean promising(int i) {
         for (int k = 1; k < i; k++) {
-            if (col[i] == col[k] || Math.abs(col[i] - col[k]) == Math.abs(i - k)) {
+            if (col[i] == col[k] || Math.abs(col[i] - col[k]) == Math.abs(i - k)) { // 몇 번째 에 뒀는지와 대각선 범위 체크
                 return false;
             }
         }
