e9b4d10d5cbb00c1886cd7c9f6ca3f852c560065
ppusda <ppusda1234@gmail.com>
2024-09-03 19:53:01 +0900

    Baekjoon: 10171_고양이

---
+++ src/BaekJoon/Bronze/Pr10171.java
@@ -0,0 +1,14 @@
+package BaekJoon.Bronze;
+
+public class Pr10171 {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\\n");
+        sb.append(" )  ( ')\n");
+        sb.append("(  /  )\n");
+        sb.append(" \\(__)|\n");
+
+        System.out.println(sb);
+    }
+}
