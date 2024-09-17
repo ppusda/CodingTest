ee527356b30d44b51197c0d9a4222b44d47d656b
ppusda <ppusda1234@gmail.com>
2024-09-17 19:23:01 +0900

    Baekjoon: 1003_피보나치 함수

---
+++ src/BaekJoon/Silver/Pr1003/Pr1003.java
@@ -0,0 +1,38 @@
+package BaekJoon.Silver.Pr1003;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Pr1003 {
+
+    private static Integer[][] dp = new Integer[41][2];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        StringBuilder sb = new StringBuilder();
+
+        while(T --> 0){
+            int N = Integer.parseInt(br.readLine());
+            fibonacci(N);
+            sb.append(dp[N][0] + " " + dp[N][1]).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    private static Integer[] fibonacci(int N) {
+        if(dp[N][0] == null || dp[N][1] == null) {
+            dp[N][0] = fibonacci(N - 1)[0] + fibonacci(N - 2)[0];
+            dp[N][1] = fibonacci(N - 1)[1] + fibonacci(N - 2)[1];
+        }
+        return dp[N];
+
+    }
+}
