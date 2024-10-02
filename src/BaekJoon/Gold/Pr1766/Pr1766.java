2015b6dd4388f49a4085d8d33617c5f039e69cdb
ppusda <ppusda1234@gmail.com>
2024-10-03 02:45:31 +0900

    Baekjoon: 1766_문제집 - 설명 보충

--- src/BaekJoon/Gold/Pr1766/Pr1766.java
+++ src/BaekJoon/Gold/Pr1766/Pr1766.java
@@ -4,17 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Collections;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Pr1766 {
 
     private static int N, M;
     private static StringBuilder sb;
-    private static ArrayList<ArrayList<Integer>> arrayList;
-    private static int[] count; // 간선 확인용
+    private static ArrayList<ArrayList<Integer>> arrayList; // 인접리스트
+    private static int[] indegree; // 진입차수
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,16 +25,17 @@ public class Pr1766 {
         arrayList = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
             arrayList.add(new ArrayList<>());
-        }
+        } // 인접리스트 초기화
 
-        count = new int[N+1];
+        indegree = new int[N+1];
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            arrayList.get(a).add(b); // a 는 b 보다 먼저 푸는게 좋음
-            count[b]++;
+            arrayList.get(a).add(b);
+            indegree[b]++;
         }
 
         sb = new StringBuilder();
@@ -44,11 +44,11 @@ public class Pr1766 {
         System.out.println(sb);
     }
 
-    private static void topologicalSort() { // 위 상 정 렬
+    private static void topologicalSort() {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for (int i = 1; i <= N; i++) {
-            if (count[i] == 0) {
+            if (indegree[i] == 0) {
                 pq.offer(i);
             }
         }
@@ -59,9 +59,9 @@ public class Pr1766 {
             ArrayList<Integer> target = arrayList.get(node);
 
             for (int t : target) {
-                count[t] --;
+                indegree[t] --;
 
-                if (count[t] == 0) {
+                if (indegree[t] == 0) {
                     pq.offer(t);
                 }
             }
