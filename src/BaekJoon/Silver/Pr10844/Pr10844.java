c7e8747f935bb7cb857204b365dfbe474bafde08
ppusda <ppusda1234@gmail.com>
2024-12-10 23:33:10 +0900

    Baekjoon: 10844_쉬운 계단 수

---
+++ src/BaekJoon/Silver/Pr10844/Pr10844.java
@@ -0,0 +1,40 @@
+package BaekJoon.Silver.Pr10844;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Pr10844 {
+
+    final static long MOD = 1000000000;
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        long[][] dp = new long[N + 1][10];
+        for(int i = 1; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for(int i = 2; i <= N; i++) {
+            for(int j = 0; j < 10; j++) {
+                if(j == 0) {
+                    dp[i][0] = dp[i - 1][1] % MOD;
+                }
+                else if (j == 9) {
+                    dp[i][9] = dp[i - 1][8] % MOD;
+                }
+                else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+                }
+            }
+        }
+
+        long result = 0;
+        for(int i = 0; i < 10; i++) {
+            result += dp[N][i];
+        }
+
+        System.out.println(result % MOD);
+    }
+}
