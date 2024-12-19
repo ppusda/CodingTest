3ad7e4fefa21a6ecbb7854b81b9ee440cf578d28
ppusda <ppusda1234@gmail.com>
2024-12-19 22:52:20 +0900

    Baekjoon: 10844_쉬운 계단 수 - 수정

--- src/BaekJoon/Silver/Pr10844/Pr10844.java
+++ src/BaekJoon/Silver/Pr10844/Pr10844.java
@@ -1,13 +1,14 @@
 package BaekJoon.Silver.Pr10844;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Pr10844 {
 
     final static long MOD = 1000000000;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
