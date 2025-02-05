8fe1ec1d2c1c2aba973793e703a47d00f4754989
ppusda <ppusda1234@gmail.com>
2025-02-05 20:23:41 +0900

    Baekjoon: 2750_수 정렬하기

---
+++ src/BaekJoon/Silver/Pr2750/Pr2750.java
@@ -0,0 +1,30 @@
+package BaekJoon.Silver.Pr2750;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Pr2750 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[N];
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		for(int i = 0; i < N - 1; i++) {
+			for(int j = i + 1; j < N; j++) {
+				if(arr[i] > arr[j]) {
+					int temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+
+		for(int val : arr) System.out.println(val);
+	}
+}
